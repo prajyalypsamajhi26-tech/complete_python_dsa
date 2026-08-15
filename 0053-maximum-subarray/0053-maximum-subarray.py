7fec9cb268bda229f4523b1ca29a45edc3c049fb
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-08-15 11:40:27 +0530

    Time: 22 ms (92.62%), Space: 31.3 MB (73.86%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -7,8 +7,7 @@ class Solution:
             curr_sum += nums[i]
             if curr_sum > max_sum:
                 max_sum = curr_sum
-            if curr_sum <=0:
+            if curr_sum <= 0:
                 curr_sum = 0
-
 
         return max_sum
