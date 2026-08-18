7206cdbfe56a109777bfeb9443dc44bb3075eea4
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-08-18 13:32:48 +0530

    Time: 94 ms (21.09%), Space: 22.3 MB (25.3%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -7,9 +7,9 @@ class Solution:
 
         start = 2
 
-        for i in range(2, n):
+        for i in range(2,n):
             if nums[i] != nums[start - 2]:
                 nums[start] = nums[i]
                 start += 1
 
-        return start
+        return start 
