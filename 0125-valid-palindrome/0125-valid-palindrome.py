e07cfe856ca88d5a3c8d6c37869a0320a8c1840b
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-08-29 23:00:53 +0530

    Time: 5 ms (87.75%), Space: 19.7 MB (53.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        final  =""
-        s=s.lower()
+        final = ""
+        s = s.lower()
 
         for ele in s:
-           if ele.isalnum():
-            final += ele
-
+            if ele.isalnum():
+                final += ele
 
         return final==final[::-1]
