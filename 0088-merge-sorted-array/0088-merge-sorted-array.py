2175568bc60c6431d928b220d3acc32f28bb3d91
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-09-08 13:39:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (77.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,18 +7,17 @@ class Solution:
         j = n-1
         k = m+n -1
 
-        while j >= 0 and i >= 0:
-
+        while j >= 0 and i >=0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -=1
+
             else:
                 nums1[k] = nums2[j]
                 j -=1
-            k -=1
+            k-=1
 
-        while j >=0 :
+        while j >= 0:
             nums1[k] = nums2[j]
             j -=1
-            k -=1
-        
+            k-=1
