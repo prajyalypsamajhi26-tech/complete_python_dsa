86549bf375c7d8eadb1cd297af81a992dc42643b
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-09-08 14:00:35 +0530

    Time: 7 ms (87.68%), Space: 20.7 MB (27.48%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,19 +3,18 @@ class Solution:
         s = list(s)
 
         left = 0
-        right = len(s) -1
+        right = len(s) - 1
 
         while left < right:
 
             if s[left] not in "aeiouAEIOU":
-                left +=1
+                left += 1
 
             elif s[right] not in "aeiouAEIOU":
                 right -=1
 
             else:
                 s[left] , s[right] = s[right] , s[left]
-
                 left += 1
                 right -=1
 
