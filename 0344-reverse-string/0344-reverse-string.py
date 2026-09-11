249dcb2895e459670ff5e0035871888efc0fecb3
Prajyalypsa Majhi <prajyalypsamajhi26@gmail.com>
2026-09-11 21:04:31 +0530

    Time: 0 ms (100%), Space: 23.6 MB (5.14%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -9,5 +9,5 @@ class Solution:
         while left < right:
             s[left] , s[right] = s[right] ,s[left]
 
-            left +=1
-            right -=1
+            left += 1
+            right -= 1
